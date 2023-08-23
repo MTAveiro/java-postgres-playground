2984f4a67c126d8cf52e2dfd37c61df4dacad10d
Matheus Tomaz de Aveiro <mathaveiro@hotmail.com>
2023-08-23 02:07:08 +0000

    testes

---
+++ src/main/java/com/example/Cliente.java
@@ -0,0 +1,47 @@
+package com.example;
+
+public class Cliente{
+   private double renda;
+   private char sexo;
+   private int anoNascimento;
+
+    public double getRenda() {
+        return renda;
+    }
+
+    public void setRenda(double renda) {
+        if (renda>=0) {
+            this.renda = renda;
+        }else{
+            System.out.println("renda deve ser maior que zero");
+
+        }
+        
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+    public void setSexo(char sexo) {
+        if (sexo == 'M' || sexo == 'F') {
+            this.sexo = sexo;
+        } else {
+            System.out.println("Sexo deve ser M ou F");
+        }
+        
+    }
+
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setAnoNascimento(int anoNascimento) {
+        if (anoNascimento>=1900) {
+            this.anoNascimento = anoNascimento;
+        }else{
+            System.out.println("Ano de nascimento deve ser maior que 1900");
+
+        }
+        this.anoNascimento = anoNascimento;
+    }
+}
---
+++ src/test/java/com/example/AppMatriz.java
@@ -0,0 +1,24 @@
+package com.example;
+
+//import java.util.Arrays;
+
+public class AppMatriz {
+   
+    public static void main(String[] args){
+        int vetor3[] = new int[5];
+        for (int i = 0; i < vetor3.length; i++) {
+            vetor3[i] = 100*(i+1);
+        }
+
+        double matriz[][] = new double[6][6];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = i*matriz[i].length+j;
+                System.out.printf("%5.1f", matriz[i][j]);
+            }
+            System.err.println();
+        }
+
+       // System.out.println(Arrays.toString(vetor3));
+    }
+}
